97e65a4dfe8fa2af91d0caa0a21ee30dd6af1940
Natalia <nataliadiasfernande@gmail.com>
2025-02-03 14:59:37 -0300

    aprendendo cores no terminal

---
+++ aula 11/cores.py
@@ -0,0 +1,16 @@
+print('\033[31mOlá Mundo!') #colocando a cor vermelha
+
+print('\033[31;43mOlá mundo!') #amarelo
+
+print('\033[1;31;43mOlá mundo!') #amarelo com negrito
+
+print('\033[1;31;43mOlá mundo!\033[m') #diminuindo a faixa final do amarelo
+
+print('\033[4;30;45mOlá mundo!\033[m') #colocando sublinhado na letra
+
+print('\033[7;30mOlá Mundo!\033[m') #letra branca com fundo preto
+
+print('\033[0;33;44mOlá mundo!\033[m') #letra branca com fundo azul
+
+
+#o 7 inverte a cor
