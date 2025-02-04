4c252f1e3db019b211bddff58ffbeb178a1afe25
Natalia <nataliadiasfernande@gmail.com>
2025-02-04 11:21:53 -0300

    mundo dois

--- mundo dois/aula.py
+++ mundo dois/aula.py
@@ -3,10 +3,10 @@
 nome = str(input('Qual é seu nome? '))
 if nome == 'Natalia':     #condição simples
     print('Que nome bonito!')
-elif nome == 'Maria' or nome == 'João' or nome =='Paulo':  #estrutura condicional alinhada
+elif nome == 'Maria' or nome == 'João' or nome =='Paulo':  #estrutura condicional aninhada
     print('Seu nome é bem popular no Brasil.')
 elif nome in 'Aurora Ariel Suzi':
     print('Belo nome feminino!')
 else:                     #estrutura condicional composta
     print('Seu nome é bem normal!')
-print('Tenha um bom dia, {}!'.format(nome))
+print('Tenha um bom dia, {}!'.format(nome))  
