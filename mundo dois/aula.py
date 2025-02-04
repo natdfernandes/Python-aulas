f13552cc8fe4a9f252b200b27b73847745805c70
Natalia <nataliadiasfernande@gmail.com>
2025-02-04 11:21:45 -0300

    condições em python

---
+++ mundo dois/aula.py
@@ -0,0 +1,12 @@
+#estrutura condicional
+
+nome = str(input('Qual é seu nome? '))
+if nome == 'Natalia':     #condição simples
+    print('Que nome bonito!')
+elif nome == 'Maria' or nome == 'João' or nome =='Paulo':  #estrutura condicional alinhada
+    print('Seu nome é bem popular no Brasil.')
+elif nome in 'Aurora Ariel Suzi':
+    print('Belo nome feminino!')
+else:                     #estrutura condicional composta
+    print('Seu nome é bem normal!')
+print('Tenha um bom dia, {}!'.format(nome))
