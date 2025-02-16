65290263d3afe9061fd2b4034183d9c5640aa6f0
Natalia <nataliadiasfernande@gmail.com>
2025-02-16 11:47:30 -0300

    desafio 38 concluido

--- mundo dois/desafio38.py
+++ mundo dois/desafio38.py
@@ -1,4 +1,18 @@
-#Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
-#o primeiro valor é maior
-#o segundo valor é maior
-#não existe valor maior os dois são iguais
+# Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
+# o primeiro valor é maior
+# o segundo valor é maior
+# não existe valor maior os dois são iguais
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+if numero1 > numero2:
+    print("O primeiro número é maior")
+elif numero2 > numero1:
+    print("O segundo número é maior")
+elif numero1 < numero2:
+    print("O número 1 é menor")
+elif numero2 < numero1:
+    ("O numero 2 é menor")
+elif numero1 == numero2:
+    print("Os números são iguais")
