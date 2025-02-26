0791cf8c1e5c636ddc18dd95b12b8a60fd6550f8
Natalia <nataliadiasfernande@gmail.com>
2025-02-26 15:58:40 -0300

    desafio 41 precisa ser corrigido

--- mundo dois/desafio41.py
+++ mundo dois/desafio41.py
@@ -1,7 +1,18 @@
-#A cofederação nacional da natação precisa de um programa que leia o ano de nascimento
-#de um atleta e mostre a sua categoria, de acordo com a idade:
-#até 9 anos: MIRIM
-#até 14 anos: INFANTIL
-#até 19 anos: JUNIOR
-#até 20 anos: SÊNIOR
-#acima: MASTER
+# A cofederação nacional da natação precisa de um programa que leia o ano de nascimento
+# de um atleta e mostre a sua categoria, de acordo com a idade:
+# até 9 anos: MIRIM
+# até 14 anos: INFANTIL
+# até 19 anos: JUNIOR
+# até 20 anos: SÊNIOR
+# acima: MASTER
+
+ano = int(input("Digite o ano de seu nascimento: "))
+anoatual = 2025 - ano
+idade = anoatual
+
+if ano <= 2016:
+    print("Você é da categoria MIRIM!")
+elif ano <= 2011:
+    print("Você é da categoriaa INFANTIL!")
+elif ano <= 2006:
+    print("Você é da categoria JUNIOR!")
