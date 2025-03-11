3e40516977e498278d12370bbf38ad41a412f048
Natalia <nataliadiasfernande@gmail.com>
2025-03-11 14:30:06 -0300

    iniciando desafio 44

--- mundo dois/desafio44.py
+++ mundo dois/desafio44.py
@@ -1,5 +1,7 @@
-#Ela bore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
-#á vista dinheiro/cheque: 10% de desconto
-#á vista no cartão: 5% de desconto
-#em até 2x no cartão: preço normal
-#3x ou mais no cartão: 20% de juros
+# Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+# á vista dinheiro/cheque: 10% de desconto
+# á vista no cartão: 5% de desconto
+# em até 2x no cartão: preço normal
+# 3x ou mais no cartão: 20% de juros
+
+valor = float(input("Digite o valor do produto a ser pago R$: "))
